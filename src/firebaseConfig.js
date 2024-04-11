d4af14741441f34cf9681a6c2451526499545ac0
ilazarom <ilazarom17@gmail.com>
2024-04-11 09:56:30 +0200

    primer commit

---
+++ src/firebaseConfig.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore/lite";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBzFOjR4pkSsgR5SpJ6enNbgcQqTuFGj4w",
+  authDomain: "vue-3-2024-84861.firebaseapp.com",
+  projectId: "vue-3-2024-84861",
+  storageBucket: "vue-3-2024-84861.appspot.com",
+  messagingSenderId: "621982719764",
+  appId: "1:621982719764:web:52f1a8d421755bc1b1ac01"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const db = getFirestore();
+
+export {auth, db};
---
+++ src/router.js
@@ -0,0 +1,32 @@
+import {createRouter, createWebHistory} from "vue-router";
+import { useUserStore } from "./stores/user";
+import Home from "./views/Home.vue";
+import Editar from "./views/Editar.vue";
+import Login from "./views/Login.vue";
+import Register from "./views/Register.vue";
+
+const requireAuth = async (to, from, next) => {
+    const userStore = useUserStore();
+    userStore.loadingSession = true;
+    const user = await userStore.currentUser();
+    if (user) {
+        next("/");
+    } else {
+        next("/login");
+    }
+    userStore.loadingSession = false;
+};
+
+const routes = [
+    { path: "/", component: Home },
+    { path: "/editar/:id", component: Editar },
+    { path: "/login", component: Login },
+    { path: "/register", component: Register },
+];
+
+const router = createRouter({
+    routes,
+    history: createWebHistory(),
+});
+
+export default router;
